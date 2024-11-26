c53c21f7a317e1c6f156226b3a45ef62a983138b
Warrenn205 <warrenspencer41@gmail.com>
2024-11-25 18:54:57 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify, json, g
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
